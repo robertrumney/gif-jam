ef6618a16365d74d7da155c7c24bf650b50787b1
Robert Rumney <robertrumney@gmail.com>
2026-02-02 00:22:25 +0200

    Update PluginEditor.h

--- PluginEditor.h
+++ PluginEditor.h
@@ -5,40 +5,39 @@
 class GifSyncVSTAudioProcessorEditor final : public juce::AudioProcessorEditor,
                                              private juce::Timer
 {
-public:
-    explicit GifSyncVSTAudioProcessorEditor(GifSyncVSTAudioProcessor&);
-    ~GifSyncVSTAudioProcessorEditor() override;
+	public:
+		explicit GifSyncVSTAudioProcessorEditor(GifSyncVSTAudioProcessor&);
+		~GifSyncVSTAudioProcessorEditor() override;
 
-    void paint(juce::Graphics&) override;
-    void resized() override;
-    void mouseDown(const juce::MouseEvent& e) override;
+		void paint(juce::Graphics&) override;
+		void resized() override;
+		void mouseDown(const juce::MouseEvent& e) override;
 
-private:
-    struct GifFrame
-    {
-        juce::Image image;
-        double endTimeSec = 0.0;
-    };
+	private:
+		struct GifFrame
+		{
+			juce::Image image;
+			double endTimeSec = 0.0;
+		};
 
-    void timerCallback() override;
+		void timerCallback() override;
 
-    void showContextMenu();
-    void loadGifFromFile(const juce::File& file);
+		void showContextMenu();
+		void loadGifFromFile(const juce::File& file);
 
-    const GifFrame* getFrameForSyncedTime(double syncedTime01) const noexcept;
+		const GifFrame* getFrameForSyncedTime(double syncedTime01) const noexcept;
 
-    GifSyncVSTAudioProcessor& processor;
+		GifSyncVSTAudioProcessor& processor;
 
-    juce::File lastGifFile;
-    juce::String statusText;
+		juce::String statusText;
 
-    juce::Array<GifFrame> frames;
-    double totalGifSec = 0.0;
+		juce::Array<GifFrame> frames;
+		double totalGifSec = 0.0;
 
-    double freeRunPhaseSec = 0.0;
-    double lastTimerSec = 0.0;
+		double freeRunPhaseSec = 0.0;
+		double lastTimerSec = 0.0;
 
-    double syncBars = 1.0;
+		double syncBars = 1.0;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GifSyncVSTAudioProcessorEditor)
+		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GifSyncVSTAudioProcessorEditor)
 };
